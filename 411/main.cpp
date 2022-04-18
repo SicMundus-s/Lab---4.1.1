96df7d9724439b23fa2b5e57187577bfd72f1aba
mrNRaven-hub <nikita.volkov0505@mail.ru>
2022-04-18 16:56:36 +0300

    Добавьте файлы проекта.

---
+++ 411/main.cpp
@@ -0,0 +1,11 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include "cl_application.h"
+
+int main()
+{
+	setlocale(LC_ALL, "RUSSIAN");
+	cl_application ob_cl_application(nullptr);
+	ob_cl_application.bild_tree_objects(); //Построение древа объектов
+	return ob_cl_application.exec_app(); //Запуск системы
+}
